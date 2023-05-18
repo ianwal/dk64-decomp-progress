3ef428d526a53a2e28866d2feb13581481448133
Donkey Kong <dk64.decomp@gmail.com>
2023-05-18 10:35:43 -0500

    21.6384%

--- src/global_asm/code_9BF30.c
+++ src/global_asm/code_9BF30.c
@@ -4,21 +4,19 @@
 // Displaylist stuff
 #pragma GLOBAL_ASM("asm/nonmatchings/global_asm/code_9BF30/func_80697230.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/global_asm/code_9BF30/func_806978FC.s")
-
-/*
-// TODO: Missing another temp variable, hmm
 extern s32 *D_807FC7C0;
 
 void func_806978FC(void) {
     s16 phi_s0;
+    s16 i;
 
+    phi_s0 = 0;
     D_807FC7C0 = malloc(0xC0);
-    for (phi_s0 = 0; phi_s0 < 0x30; phi_s0++) {
+    for (i = 0; i < 0x30; i++) {
         D_807FC7C0[phi_s0] = getPointerTableFile(0x19, phi_s0 + 0x1654, 1, 1);
+        phi_s0++;
     }
 }
-*/
 
 #pragma GLOBAL_ASM("asm/nonmatchings/global_asm/code_9BF30/func_80697988.s")
 
