f0bd26b635240bc839da91f6894beeb72365762f
Donkey Kong <dk64.decomp@gmail.com>
2023-05-19 10:29:54 -0500

    21.9825%

--- src/global_asm/code_14CF0.c
+++ src/global_asm/code_14CF0.c
@@ -14,14 +14,10 @@ void func_8060FFF0(void) {
 
 #pragma GLOBAL_ASM("asm/nonmatchings/global_asm/code_14CF0/func_80610044.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/global_asm/code_14CF0/func_80610268.s")
-
-/*
 extern OSMesgQueue D_807659E8;
 extern s8 D_80746834;
 extern s8 D_807F059C;
 
-// TODO: Everything matches except for a bne where s1 and t8 are swapped, no idea why
 void func_80610268(s32 arg0) {
     u32 sp34;
 
@@ -30,7 +26,6 @@ void func_80610268(s32 arg0) {
         osRecvMesg(&D_807659E8, &sp34, 1);
         D_80746834 = 0;
         (&D_807F059C)[sp34 >> 0x10] = 0;
-        sp34 = (u16)sp34;
-    } while (sp34 != arg0);
+        sp34 &= 0xFFFF;
+    } while ((s32)sp34 != arg0);
 }
-*/
