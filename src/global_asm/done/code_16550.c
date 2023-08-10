62de90ee2eb4abcf890f0ab6ef1fe433b4c10eb7
Donkey Kong <dk64.decomp@gmail.com>
2023-08-10 13:35:41 +0930

    .rodata OK for code_16550

--- src/global_asm/done/code_16550.c
+++ src/global_asm/done/code_16550.c
@@ -2,12 +2,6 @@
 #include "functions.h"
 
 u32 func_global_asm_80611DA0(s16);
-extern f32 D_global_asm_807572D0;
-extern f32 D_global_asm_807572D4;
-extern f32 D_global_asm_807572D8;
-extern f32 D_global_asm_807572DC;
-extern f32 D_global_asm_807572E0;
-extern f32 D_global_asm_807572E4;
 
 f32 func_global_asm_80611850(f32 arg0) {
     s16 var_a0;
@@ -17,9 +11,9 @@ f32 func_global_asm_80611850(f32 arg0) {
     } else if (arg0 <= -1.0f) {
         var_a0 = -0x7FFF;
     } else {
-        var_a0 = arg0 * D_global_asm_807572D0;
+        var_a0 = arg0 * 32767.0f;
     }
-    return (func_global_asm_80611DA0(var_a0) * D_global_asm_807572D4) / D_global_asm_807572D8;
+    return (func_global_asm_80611DA0(var_a0) * 3.141592741f) / 65535.0f;
 }
 
 f32 func_global_asm_806118FC(f32 arg0) {
@@ -30,7 +24,7 @@ f32 func_global_asm_806118FC(f32 arg0) {
     } else if (arg0 <= -1.0f) {
         phi_a0 = -0x7FFF;
     } else {
-        phi_a0 = arg0 * D_global_asm_807572DC;
+        phi_a0 = arg0 * 32767.0f;
     }
-    return (func_global_asm_80611DF4(phi_a0) * D_global_asm_807572E0) / D_global_asm_807572E4;
+    return (func_global_asm_80611DF4(phi_a0) * 3.141592741f) / 65535.0f;
 }
