59df44a27849312ca1e629e392b8ab491053a548
Donkey Kong <dk64.decomp@gmail.com>
2023-12-14 22:56:31 +1030

    34.0368%

--- src/bonus/code_9010.c
+++ src/bonus/code_9010.c
@@ -1,10 +1,6 @@
 #include <ultra64.h>
 #include "functions.h"
 
-// .rodata
-// const char D_bonus_8002DED4[] = "SCORE";
-// const char D_bonus_8002DEDC[] = "LAP BONUS";
-
 extern s8 D_bonus_8002D910[];
 extern s8 D_bonus_8002D91C[];
 extern s8 D_bonus_8002D920;
@@ -80,7 +76,123 @@ Gfx *func_bonus_8002D010(Gfx *dl, Actor *arg1) {
     return dl;
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/bonus/code_9010/func_bonus_8002D2F0.s")
+extern s16 D_80744518;
+extern u16 D_807ECD60;
+
+typedef struct {
+    s16 unk0;
+    u8 unk2;
+    u8 unk3;
+    s16 unk4;
+    u8 unk6;
+    u8 unk7;
+    u8 unk8;
+    u8 unk9;
+    s16 unkA;
+    u8 unkC;
+    u8 unkD;
+} AAD_8002D2F0;
+
+s8 func_global_asm_806FDB8C(s32, void*, s32, f32, f32, f32);
+s32 func_global_asm_806A2A10(s32, s32, u8);
+
+void func_global_asm_8061CB08(void);
+
+void func_bonus_8002D2F0(void) {
+    s32 var_a0;
+    u32 sp28;
+    AAD_8002D2F0 *aaD;
+
+    aaD = current_actor_pointer->additional_actor_data;
+    if (!(current_actor_pointer->object_properties_bitfield & 0x10)) {
+        playCutscene(NULL, 0, 1);
+        current_actor_pointer->control_state = 0;
+        aaD->unk4 = 0x3C;
+        D_bonus_8002D92C = 0;
+        D_bonus_8002D930 = 0;
+        D_80744518 = 0;
+        aaD->unk3 = func_global_asm_806FDB8C(1, "SCORE", 8, 0.0f, 0.0f, 0.0f);
+        aaD->unkD = func_global_asm_806FDB8C(1, "LAP BONUS", 8, 0.0f, 0.0f, 0.0f);
+        D_bonus_8002D920 = 0;
+        D_bonus_8002D924 = 0;
+        D_bonus_8002D928 = 0;
+    }
+    switch (current_actor_pointer->control_state) {
+        case 0:
+            if (is_cutscene_active == 1) {
+                if (func_global_asm_8061CB38() != 0) {
+                    switch (current_map) {
+                        case MAP_RAMBI_ARENA:
+                            sp28 = 0x1F;
+                            break;
+                        case MAP_ENGUARDE_ARENA:
+                            sp28 = 0x20;
+                            break;
+                    }
+                    func_global_asm_8070D8C0(current_actor_pointer, 0, sp28);
+                    current_actor_pointer->control_state = 1;
+                    func_global_asm_806A2A10(0xDC, 0x2A, aaD->unk4);
+                } else if ((D_global_asm_807ECD58.unk0 & 0x8000) && !(D_807ECD60 & 0x8000)) {
+                    playCutscene(player_pointer, 1, 1);
+                    current_actor_pointer->control_state = 2;
+                    func_global_asm_806A2A10(0xDC, 0x2A, aaD->unk4);
+                    func_global_asm_806A2B08(current_actor_pointer->unk11C);
+                }
+            }
+            break;
+        case 1:
+            if (!(current_actor_pointer->object_properties_bitfield & 0x02000000) || ((D_global_asm_807ECD58.unk0 & 0x8000) && !(D_807ECD60 & 0x8000))) {
+                func_global_asm_8061CB08();
+                current_actor_pointer->control_state = 2;
+                func_global_asm_806A2B08(current_actor_pointer->unk11C);
+            }
+            break;
+        case 2:
+            if (current_actor_pointer->unk11C->control_state == 5) {
+                D_bonus_8002D930 = 0;
+                current_actor_pointer->control_state = 3;
+                aaD->unk0 = 0x78;
+            }
+            switch (current_map) { // irregular
+                case MAP_RAMBI_ARENA:
+                    D_bonus_8002D930--;
+                    if (D_bonus_8002D930 <= 0) {
+                        D_bonus_8002D930 = 0;
+                        if (D_bonus_8002D92C > 0) {
+                            var_a0 = 0;
+                            if (D_bonus_8002D92C >= 2) {
+                                var_a0 = 1;
+                            }
+                            while (D_bonus_8002D92C > 0) {
+                                D_bonus_8002D92C -= 1;
+                                aaD->unkA += (D_bonus_8002DEF0[D_bonus_8002D92C] << var_a0);
+                            }
+                        }
+                    }
+                    break;
+                case MAP_ENGUARDE_ARENA:
+                    if (D_bonus_8002D928 != 0) {
+                        aaD->unkA += D_bonus_8002D928;
+                        if (D_bonus_8002D928 >= 0x33) {
+                            aaD->unkC = 0x3C;
+                        }
+                        D_bonus_8002D928 = 0;
+                    }
+                    break;
+            }
+            break;
+        case 3:
+            current_actor_pointer->unk11C->control_state = 1;
+            aaD->unk0--;
+            if (aaD->unk0 == 0) {
+                func_global_asm_805FF378(MAP_MAIN_MENU, 0);
+                current_actor_pointer->control_state = 4;
+            }
+            break;
+    }
+    D_80744518 = aaD->unkA;
+    func_global_asm_8068C350(func_bonus_8002D010, current_actor_pointer, 3);
+}
 
 void func_bonus_8002D6F8(s16 arg0) {
     s32 var_v1_2;
