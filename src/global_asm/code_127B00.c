47839793cf009b2974201b1ca2f2bfcee42269f9
Donkey Kong <dk64.decomp@gmail.com>
2024-03-04 16:04:40 +1030

    127B00 rodata

--- src/global_asm/code_127B00.c
+++ src/global_asm/code_127B00.c
@@ -1,6 +1,16 @@
 #include <ultra64.h>
 #include "functions.h"
 
+// rodata
+/*
+static const f64 D_global_asm_8075EB40 = 300.0;
+static const f32 D_global_asm_8075EB48 = 999999.0f;
+static const f64 D_global_asm_8075EB50 = 0.0050000000000000001;
+static const f32 D_global_asm_8075EB58 = 999999.0f;
+static const f64 D_global_asm_8075EB60 = 0.0500000000000000028;
+static const f64 D_global_asm_8075EB70 = 0.400000000000000022;
+static const f64 D_global_asm_8075EB78 = 3.14159274101257324;
+*/
 
 void func_global_asm_80722E00(s32 map) {
     void *paths;
@@ -99,6 +109,7 @@ void func_global_asm_80723428(s32 arg0) {
     }
 }
 
+// rodata
 #pragma GLOBAL_ASM("asm/nonmatchings/global_asm/code_127B00/func_global_asm_80723484.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/global_asm/code_127B00/func_global_asm_807238D4.s")
@@ -117,12 +128,53 @@ u8 func_global_asm_80723C98(s32 arg0) {
 
 #pragma GLOBAL_ASM("asm/nonmatchings/global_asm/code_127B00/func_global_asm_80724154.s")
 
+f32 func_global_asm_80724308(s32 arg0, f32 arg1, f32 arg2, f32 arg3, s32 arg4, f32 arg5);
+
+/*
+void func_global_asm_80724154(s32 arg0, f32 arg1, f32 arg2, f32 arg3, s32 *arg4, f32 *arg5) {
+    f32 test;
+    f32 var_f22;
+    f32 highest;
+    s32 var_s0;
+    s32 i;
+
+    highest = 999999.0f;
+    var_s0 = *arg4;
+    var_f22 = *arg5 - 0.5;
+    i = 0x14;
+    if (var_f22 < 0.0) {
+        if (var_s0 > 0) {
+            var_s0--;
+            var_f22 += 1.0f;
+        } else {
+            var_f22 = 0.0f;
+        }
+    }
+    while (i-- != 0) {
+        test = func_global_asm_80724308(arg0, arg1, arg2, arg3, var_s0, var_f22);
+        if (test < highest) {
+            *arg4 = var_s0;
+            *arg5 = var_f22;
+            highest = test;
+        }
+        var_f22 += 0.05;
+        if (var_f22 >= 1.0f && var_s0 < D_global_asm_807FDB90[arg0]->unk4) {
+            var_s0++;
+            var_f22 -= 1.0f;
+        } else if (var_f22 >= 1.0f) {
+            var_f22 = 1.0f;
+        }
+    }
+}
+*/
+
 #pragma GLOBAL_ASM("asm/nonmatchings/global_asm/code_127B00/func_global_asm_80724308.s")
 
 s16 func_global_asm_807245D8(s32 arg0) {
     return D_global_asm_807FDB90[arg0]->unk4;
 }
 
+// rodata
 #pragma GLOBAL_ASM("asm/nonmatchings/global_asm/code_127B00/func_global_asm_807245F0.s")
 
 void func_global_asm_80724874(Actor *arg0) {
