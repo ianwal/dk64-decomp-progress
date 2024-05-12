45984fe1e160088a7296dcac96137fad1e3465ba
Donkey Kong <dk64.decomp@gmail.com>
2024-05-13 08:09:18 +0930

    Rodata for dk64_boot_3D20

--- src/dk64_boot/dk64_boot_3D20.c
+++ src/dk64_boot/dk64_boot_3D20.c
@@ -1,6 +1,11 @@
 #include <ultra64.h>
 #include "functions.h"
 
+const char D_dk64_boot_80010310[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+const char D_dk64_boot_80010338[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const char D_dk64_boot_80010360[] = "";
+const char D_dk64_boot_80010364[] = "(null)";
+const char D_dk64_boot_8001036C[] = "(nil)";
 
 #pragma GLOBAL_ASM("asm/nonmatchings/dk64_boot/dk64_boot_3D20/func_dk64_boot_80003120.s")
 
