218f73c1cb591ffc0f34e717e0071e2b4621c857
Donkey Kong <dk64.decomp@gmail.com>
2024-07-02 09:25:41 +0930

    43.1744%

--- src/dk64_boot/dk64_boot_CA20.c
+++ src/dk64_boot/dk64_boot_CA20.c
@@ -5,72 +5,92 @@
 #include <rcp.h>
 #include "piint.h"
 
-#pragma GLOBAL_ASM("asm/nonmatchings/dk64_boot/dk64_boot_CA20/osEPiRawStartDma.s")
+extern OSPiHandle *__osCurrentHandle[];
 
-/*
-#define OS_RAMROM_STACKSIZE 1024
+#define WAIT_ON_IOBUSY(stat)                                                                \
+    {                                                                                       \
+        stat = IO_READ(PI_STATUS_REG);                                                      \
+        while (stat & (PI_STATUS_IO_BUSY | PI_STATUS_DMA_BUSY))                             \
+            stat = IO_READ(PI_STATUS_REG);                                                  \
+    } (void)0
 
-#ifndef WAIT_ON_IOBUSY
-#define WAIT_ON_IOBUSY(stat)                                \
-    stat = IO_READ(PI_STATUS_REG);                          \
-    while (stat & (PI_STATUS_IO_BUSY | PI_STATUS_DMA_BUSY)) \
-        stat = IO_READ(PI_STATUS_REG);
-#endif
-
-#define UPDATE_REG(reg, var)           \
+#define UPDATE_REG(pihandle, reg, var) \
     if (cHandle->var != pihandle->var) \
-        IO_WRITE(reg, pihandle->var);
+        IO_WRITE(reg, pihandle->var)
 
-#define EPI_SYNC(pihandle, stat, domain)                  \
-                                                          \
-    WAIT_ON_IOBUSY(stat)                                  \
-                                                          \
-    domain = pihandle->domain;                            \
-    if (__osCurrentHandle[domain] != pihandle)            \
-    {                                                     \
-        OSPiHandle *cHandle = __osCurrentHandle[domain];  \
-        if (domain == PI_DOMAIN1)                         \
-        {                                                 \
-            UPDATE_REG(PI_BSD_DOM1_LAT_REG, latency);     \
-            UPDATE_REG(PI_BSD_DOM1_PGS_REG, pageSize);    \
-            UPDATE_REG(PI_BSD_DOM1_RLS_REG, relDuration); \
-            UPDATE_REG(PI_BSD_DOM1_PWD_REG, pulse);       \
-            UPDATE_REG(PI_BSD_DOM1_PGS_REG, domain);       \
-        }                                                 \
-        else                                              \
-        {                                                 \
-            UPDATE_REG(PI_BSD_DOM2_LAT_REG, latency);     \
-            UPDATE_REG(PI_BSD_DOM2_PGS_REG, pageSize);    \
-            UPDATE_REG(PI_BSD_DOM2_RLS_REG, relDuration); \
-            UPDATE_REG(PI_BSD_DOM2_PWD_REG, pulse);       \
-            UPDATE_REG(PI_BSD_DOM2_PGS_REG, domain);       \
-        }                                                 \
-        __osCurrentHandle[domain] = pihandle;             \
-    }
-*/
+#define EPI_SYNC(pihandle, stat, domain)                             \
+                                                                     \
+    WAIT_ON_IOBUSY(stat);                                            \
+                                                                     \
+    domain = pihandle->domain;                                       \
+    if (__osCurrentHandle[domain]->type != pihandle->type)           \
+    {                                                                \
+        OSPiHandle *cHandle = __osCurrentHandle[domain];             \
+        if (domain == PI_DOMAIN1)                                    \
+        {                                                            \
+            UPDATE_REG(pihandle, PI_BSD_DOM1_LAT_REG, latency);      \
+            UPDATE_REG(pihandle, PI_BSD_DOM1_PGS_REG, pageSize);     \
+            UPDATE_REG(pihandle, PI_BSD_DOM1_RLS_REG, relDuration);  \
+            UPDATE_REG(pihandle, PI_BSD_DOM1_PWD_REG, pulse);        \
+        }                                                            \
+        else                                                         \
+        {                                                            \
+            UPDATE_REG(pihandle, PI_BSD_DOM2_LAT_REG, latency);      \
+            UPDATE_REG(pihandle, PI_BSD_DOM2_PGS_REG, pageSize);     \
+            UPDATE_REG(pihandle, PI_BSD_DOM2_RLS_REG, relDuration);  \
+            UPDATE_REG(pihandle, PI_BSD_DOM2_PWD_REG, pulse);        \
+        }                                                            \
+        cHandle->type = pihandle->type;                              \
+        cHandle->latency = pihandle->latency;                        \
+        cHandle->pageSize = pihandle->pageSize;                      \
+        cHandle->relDuration = pihandle->relDuration;                \
+        cHandle->pulse = pihandle->pulse;                            \
+    }(void)0
 
-/*
-// TODO: I think we need the 2.0I version of EPI_SYNC
-s32 osEPiRawStartDma(OSPiHandle *pihandle, s32 direction, u32 devAddr, void *dramAddr, u32 size)
-{
+s32 osEPiRawStartDma(OSPiHandle* pihandle, s32 direction, u32 devAddr, void* dramAddr, u32 size) {
     u32 stat;
     u32 domain;
 
+#ifdef _DEBUG
+    if ((direction != OS_READ) && (direction != OS_WRITE)) {
+        __osError(ERR_OSPIRAWSTARTDMA_DIR, 1, direction);
+        return -1;
+    }
+
+    if (devAddr & 0x1) {
+        __osError(ERR_OSPIRAWSTARTDMA_DEVADDR, 1, devAddr);
+        return -1;
+    }
+
+    if ((u32)dramAddr & 0x7) {
+        __osError(ERR_OSPIRAWSTARTDMA_ADDR, 1, dramAddr);
+        return -1;
+    }
+
+    if (size & 0x1) {
+        __osError(ERR_OSPIRAWSTARTDMA_SIZE, 1, size);
+        return -1;
+    }
+
+    if ((size == 0) || (size > (16 * 1024 * 1024))) {
+        __osError(ERR_OSPIRAWSTARTDMA_RANGE, 1, size);
+        return -1;
+    }
+#endif
+
     EPI_SYNC(pihandle, stat, domain);
-    
     IO_WRITE(PI_DRAM_ADDR_REG, osVirtualToPhysical(dramAddr));
     IO_WRITE(PI_CART_ADDR_REG, K1_TO_PHYS(pihandle->baseAddress | devAddr));
-    switch (direction)
-    {
-    case OS_READ:
-        IO_WRITE(PI_WR_LEN_REG, size - 1);
-        break;
-    case OS_WRITE:
-        IO_WRITE(PI_RD_LEN_REG, size - 1);
-        break;
-    default:
-        return -1;
+
+    switch (direction) {
+        case OS_READ:
+            IO_WRITE(PI_WR_LEN_REG, size - 1);
+            break;
+        case OS_WRITE:
+            IO_WRITE(PI_RD_LEN_REG, size - 1);
+            break;
+        default:
+            return -1;
     }
     return 0;
 }
-*/
