c147a0caab5c8ba96ed65045c9add96285a7df79
Donkey Kong <dk64.decomp@gmail.com>
2024-08-31 21:59:01 -0500

    43.2880%

--- src/dk64_boot/dk64_boot_3D20.c
+++ src/dk64_boot/dk64_boot_3D20.c
@@ -1,4 +1,5 @@
 #include <ultra64.h>
+#include <stdarg.h>
 #include "functions.h"
 
 const char D_dk64_boot_80010310[] = "0123456789abcdefghijklmnopqrstuvwxyz";
@@ -9,13 +10,15 @@ const char D_dk64_boot_8001036C[] = "(nil)";
 
 #pragma GLOBAL_ASM("asm/nonmatchings/dk64_boot/dk64_boot_3D20/func_dk64_boot_80003120.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/dk64_boot/dk64_boot_3D20/sprintf.s")
-
-/*
 int sprintf(char *str, const char *format, ...) {
-    func_dk64_boot_8000320C(str, format);
+    s32 ret;
+    va_list args;
+    va_start(args, format);
+
+    ret = func_dk64_boot_8000320C(str, format, args);
+    va_end(args);
+    return ret;
 }
-*/
 
 // Jumptable
 #pragma GLOBAL_ASM("asm/nonmatchings/dk64_boot/dk64_boot_3D20/func_dk64_boot_8000320C.s")
