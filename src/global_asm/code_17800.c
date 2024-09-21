d588a0610c7b18009dc3493ce8a3af951cf97ad5
Donkey Kong <dk64.decomp@gmail.com>
2024-09-21 04:44:32 -0500

    41.7045%

--- src/global_asm/code_17800.c
+++ src/global_asm/code_17800.c
@@ -1,16 +1,79 @@
 #include <ultra64.h>
 #include "functions.h"
 
+f32 func_global_asm_80612B00(f32 arg0, s16 arg1) {
+    f32 var_f2;
+    s16 i;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/global_asm/code_17800/func_global_asm_80612B00.s")
+    var_f2 = 1.0f;
+    if (arg1) {
+        for (i = 0; i < arg1; i++) {
+            var_f2 *= arg0;
+        }
+    }
+    return var_f2;
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/global_asm/code_17800/func_global_asm_80612B48.s")
+f32 func_global_asm_80612B48(s16 arg0) {
+    s16 i;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/global_asm/code_17800/func_global_asm_80612BC0.s")
+    i = 0;
+    if (arg0 != 0) {
+        do {
+            arg0 /= 10;
+            i++;
+        } while (arg0 != 0);
+    }
+    return i - 1;
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/global_asm/code_17800/func_global_asm_80612C30.s")
+extern f32 D_global_asm_80757310;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/global_asm/code_17800/func_global_asm_80612CA0.s")
+void func_global_asm_80612BC0(f32 (*arg0)[4], f32 arg1) {
+    f32 sp24;
+    f32 sp20;
+
+    arg1 *= D_global_asm_80757310;
+    sp24 = func_global_asm_80612D1C(arg1);
+    sp20 = func_global_asm_80612D10(arg1);
+    guMtxIdentF(arg0);
+    arg0[1][2] = sp24;
+    arg0[2][1] = -sp24;
+    arg0[1][1] = sp20;
+    arg0[2][2] = sp20;
+}
+
+extern f32 D_global_asm_80757314;
+
+void func_global_asm_80612C30(f32 (*arg0)[4], f32 arg1) {
+    f32 sp24;
+    f32 sp20;
+
+    arg1 *= D_global_asm_80757314;
+    sp24 = func_global_asm_80612D1C(arg1);
+    sp20 = func_global_asm_80612D10(arg1);
+    guMtxIdentF(arg0);
+    arg0[2][0] = sp24;
+    arg0[0][2] = -sp24;
+    arg0[0][0] = sp20;
+    arg0[2][2] = sp20;
+}
+
+extern f32 D_global_asm_80757318;
+
+void func_global_asm_80612CA0(f32 (*arg0)[4], f32 arg1) {
+    f32 sp24;
+    f32 sp20;
+
+    arg1 *= D_global_asm_80757318;
+    sp24 = func_global_asm_80612D1C(arg1);
+    sp20 = func_global_asm_80612D10(arg1);
+    guMtxIdentF(arg0);
+    arg0[0][1] = sp24;
+    arg0[1][0] = -sp24;
+    arg0[0][0] = sp20;
+    arg0[1][1] = sp20;
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/global_asm/code_17800/func_global_asm_80612D10.s")
 
