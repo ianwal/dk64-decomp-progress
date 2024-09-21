1ae2b0f72533f3f5a7b25afea7e64d7ac4682a30
Donkey Kong <dk64.decomp@gmail.com>
2024-09-21 16:16:47 -0500

    38.7423%

--- src/global_asm/code_166A0.c
+++ src/global_asm/code_166A0.c
@@ -16,6 +16,19 @@ s32 rand(void) {
     return func_global_asm_806119A0() & 0x7FFFFFFF;
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/global_asm/code_166A0/func_global_asm_80611A70.s")
+void func_global_asm_80611A70(f32 arg0, f32 arg1, f32 *arg2, f32 *arg3) {
+    if ((*arg2 - arg0 < 0.25) && (*arg2 - arg0 >= 0.0)) {
+        *arg2 += 0.125;
+    }
+    if ((*arg3 - arg1 < 0.25) && (*arg3 - arg1 >= 0.0)) {
+        *arg3 += 0.125;
+    }
+    if ((*arg2 - arg0 > -0.25) && (*arg2 - arg0 <= 0.0)) {
+        *arg2 -= 0.125;
+    }
+    if ((*arg3 - arg1 > -0.25) && (*arg3 - arg1 <= 0.0)) {
+        *arg3 -= 0.125;
+    }
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/global_asm/code_166A0/func_global_asm_80611BB4.s")
