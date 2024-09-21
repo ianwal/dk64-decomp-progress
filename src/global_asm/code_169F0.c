c919c5b9a1b92407a206ded80729638b3a689e14
Donkey Kong <dk64.decomp@gmail.com>
2024-09-21 16:23:25 -0500

    38.7644%

--- src/global_asm/code_169F0.c
+++ src/global_asm/code_169F0.c
@@ -6,4 +6,6 @@ f32 func_global_asm_80611CF0(f32 arg0) {
     return (s32) arg0;
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/global_asm/code_169F0/func_global_asm_80611D08.s")
+s32 func_global_asm_80611D08(f32 arg0) {
+    return arg0;
+}
