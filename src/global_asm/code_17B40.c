13308c8bac81bbfaf7aac42fa216e0f48cf93e71
Donkey Kong <dk64.decomp@gmail.com>
2024-09-21 04:29:03 -0500

    41.6724%

--- src/global_asm/code_17B40.c
+++ src/global_asm/code_17B40.c
@@ -1,5 +1,14 @@
 #include <ultra64.h>
 #include "functions.h"
 
+extern f32 D_global_asm_80757350;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/global_asm/code_17B40/func_global_asm_80612E40.s")
+f32 func_global_asm_80612E40(f32 arg0) {
+    s32 temp_f4;
+
+    temp_f4 = arg0;
+    if (D_global_asm_80757350 < (arg0 - (f32) temp_f4)) {
+        temp_f4++;
+    }
+    return temp_f4;
+}
