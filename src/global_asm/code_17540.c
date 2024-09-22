9f0cb91b209f3f04a3d8aa603b7d848ba7030f0b
Donkey Kong <dk64.decomp@gmail.com>
2024-09-21 22:00:48 -0500

    39.0283%

--- src/global_asm/code_17540.c
+++ src/global_asm/code_17540.c
@@ -14,4 +14,9 @@
 
 #pragma GLOBAL_ASM("asm/nonmatchings/global_asm/code_17540/func_global_asm_80612A54.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/global_asm/code_17540/func_global_asm_80612AD8.s")
+
+void func_global_asm_80612AD8(f32 *arg0, f32 arg1, f32 arg2, f32 arg3) {
+    arg0[0] = arg1;
+    arg0[1] = arg2;
+    arg0[2] = arg3;
+}
