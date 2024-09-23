1133edac5a6e9c10611874aff82c1462afd6fcb0
Donkey Kong <dk64.decomp@gmail.com>
2024-09-23 13:14:52 -0500

    39.4454%

--- src/global_asm/audio/code_141C00.c
+++ src/global_asm/audio/code_141C00.c
@@ -21,6 +21,20 @@ void func_global_asm_8073CF00(Struct8073F1E4_container *arg0, u8 arg1) {
     }
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/global_asm/audio/code_141C00/func_global_asm_8073CFB0.s")
+void func_global_asm_8073CFB0(Struct8073F1E4_container *arg0, u8 arg1) {
+    ALParam *sp1C;
+
+    if (arg0->unk8 != NULL) {
+        sp1C = __n_allocParam();
+        if (sp1C == NULL) {
+            return;
+        }
+        sp1C->delta = n_syn->paramSamples + arg0->unk8->unk88;
+        sp1C->type = 0x11;
+        sp1C->data.i = arg1;
+        sp1C->next = 0;
+        func_global_asm_8073F1E4(arg0->unk8, 3, sp1C);
+    }
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/global_asm/audio/code_141C00/func_global_asm_8073D060.s")
