85c7839b7952a28891062083bc5c41f6797d75a4
Donkey Kong <dk64.decomp@gmail.com>
2024-09-25 11:09:10 -0500

    40.1209%

--- src/global_asm/audio/code_137C10.c
+++ src/global_asm/audio/code_137C10.c
@@ -15,9 +15,98 @@
 // Jumptable
 #pragma GLOBAL_ASM("asm/nonmatchings/global_asm/audio/code_137C10/func_global_asm_80733D8C.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/global_asm/audio/code_137C10/func_global_asm_80735624.s")
+extern void func_global_asm_80735958(ALCSPlayer *, f32);
+extern void  func_global_asm_80735864(ALEventQueue *, ALEventListItem *);
 
-#pragma GLOBAL_ASM("asm/nonmatchings/global_asm/audio/code_137C10/func_global_asm_80735864.s")
+void func_global_asm_80735624(ALCSPlayer *seqp, ALEvent *event) {
+    // __CSPHandleMetaMsg
+    ALTempoEvent    *tevt = &event->msg.tempo;
+    // ALEvent         evt;
+    s32             tempo;
+    s32             oldUspt;
+    u32             ticks;
+    ALMicroTime         tempDelta,curDelta = 0;
+    ALEventListItem     *thisNode,*nextNode,*firstTemp = 0;
+
+
+    if (event->msg.tempo.status == AL_MIDI_Meta)
+    {
+        if (event->msg.tempo.type == AL_MIDI_META_TEMPO)
+        {
+	    oldUspt = seqp->uspt;
+	    tempo = (tevt->byte1 << 16) | (tevt->byte2 <<  8) | (tevt->byte3 <<  0);
+	    func_global_asm_80735958(seqp, (f32)tempo);	/* sct 1/8/96 */
+
+	    thisNode = (ALEventListItem*)seqp->evtq.allocList.next;
+	    while(thisNode)
+	    {
+		curDelta += thisNode->delta;
+		nextNode = (ALEventListItem*)thisNode->node.next;
+		if(thisNode->evt.type == AL_CSP_NOTEOFF_EVT)
+		{
+		    alUnlink((ALLink*)thisNode);
+
+		    if(firstTemp)
+			alLink((ALLink*)thisNode,(ALLink*)firstTemp);
+		    else
+		    {
+			thisNode->node.next = 0;
+			thisNode->node.prev = 0;
+			firstTemp = thisNode;
+		    }
+		    tempDelta = curDelta;         /* record the current delta */
+		    if(nextNode) /* don't do this if no nextNode */
+		    {
+			curDelta -= thisNode->delta;  /* subtract out this delta */
+			nextNode->delta += thisNode->delta; /* add it to next event */
+		    }
+		    thisNode->delta = tempDelta; /* set this event delta from current */
+		}
+		thisNode = nextNode;
+	    }
+
+	    thisNode = firstTemp;
+	    while(thisNode)
+	    {
+		nextNode = (ALEventListItem*)thisNode->node.next;
+		ticks = thisNode->delta/oldUspt;
+		thisNode->delta = ticks * seqp->uspt;
+		func_global_asm_80735864(&seqp->evtq,thisNode);
+		thisNode = nextNode;
+	    }
+        }
+    }
+}
+
+void  func_global_asm_80735864(ALEventQueue *evtq, ALEventListItem *item) {
+    // __CSPRepostEvent
+    OSIntMask           mask;
+    ALLink              *node;
+    ALEventListItem     *nextItem;
+    
+    mask = osSetIntMask(OS_IM_NONE);
+
+    for (node = &evtq->allocList; node != 0; node = node->next)
+    {
+        if (!node->next)  /* end of the list */
+        {
+            alLink((ALLink *)item, node);
+            break;
+        }
+        else
+        {
+            nextItem = (ALEventListItem *)node->next;
+            if (item->delta < nextItem->delta)
+            {
+                nextItem->delta -= item->delta;
+                alLink((ALLink *)item, node);
+                break;
+            }
+            item->delta -= nextItem->delta;
+        }
+    }
+    osSetIntMask(mask);
+}
 
 typedef struct {
     s32 unk0;
