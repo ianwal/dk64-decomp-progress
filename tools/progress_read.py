1ff47df65f1c32adcf4d78d19f9f7134030da51b
Donkey Kong <dk64.decomp@gmail.com>
2025-01-30 10:14:56 +1030

    beef up progress reporting

--- tools/progress_read.py
+++ tools/progress_read.py
@@ -2,6 +2,10 @@ import argparse
 import csv
 import anybadge
 
+# This unused import makes the yaml library faster. don't remove
+import pylibyaml  # pyright: ignore
+import yaml
+
 # Read using `mips-linux-gnu-readelf -S`
 overlay_sizes = {
     'dk64_boot'   : (0xE8B0 - 0x1000),
@@ -13,9 +17,9 @@ overlay_sizes = {
     'menu'        : 0x00ef50,
     'multiplayer' : 0x002f70,
     'minecart'    : 0x004b90,
-    'race'        : 0x00bb10,
     'bonus'       : 0x009860,
-    'critter'       : 0x0057f0,
+    'race'        : 0x00bb10,
+    'critter'     : 0x0057f0,
     'arcade'      : 0x00e220,
     'jetpac'      : 0x007090,
     'boss'        : 0x0118b0,
@@ -28,6 +32,58 @@ def RGB_to_hex(RGB):
     return "#"+"".join(["0{0:x}".format(v) if v < 16 else
                         "{0:x}".format(v) for v in RGB])
 
+def writeDataBadges(overlay, type, migratedBytes, totalBytes):
+    percent = ((migratedBytes/totalBytes) * 100)
+    green = min(255, round(min(1, (percent / 100) * 2) * 192))
+    red = min(255, round(min(1, ((100 - percent) / 100) * 2) * 192))
+    color = RGB_to_hex([red, green, 0])
+    badge = anybadge.Badge('.' + type, str(migratedBytes) + "/" + str(totalBytes) + " bytes - " + ("%3.4f%%" % (percent)), default_color=color)
+    badge.write_badge('progress/' + type + '_percent_' + overlay + '.svg',overwrite=True)
+
+def computeDataOverlays():
+    with open('decompressed.us.yaml') as f:
+        config = yaml.load(f.read(), Loader=yaml.SafeLoader)
+        if 'segments' in config:
+            for segmentIndex, segment in enumerate(config['segments']):
+                if 'name' in segment and segment['name'] in overlay_sizes:
+                    if 'subsegments' in segment:
+                        data =   {'found': False, 'start': 0, 'migrated': 0, 'not_migrated': 0}
+                        rodata = {'found': False, 'start': 0, 'migrated': 0, 'not_migrated': 0}
+                        for subsegmentIndex, subseg in enumerate(segment['subsegments']):
+                            # Get next address
+                            next_offset = 0x21DE360 # End of ROM (fallback)
+                            if (subsegmentIndex + 1) < len(segment['subsegments']):
+                                next_subseg = segment['subsegments'][subsegmentIndex + 1]
+                                next_offset = next_subseg['start' if 'start' in next_subseg else 0]
+                            elif (segmentIndex + 1) < len(config['segments']):
+                                next_seg = config['segments'][segmentIndex + 1]
+                                next_offset = next_seg['start' if 'start' in next_seg else 0]
+                            offset = subseg['start' if 'start' in subseg else 0]
+                            type = subseg['type' if 'type' in subseg else 1]
+                            size = next_offset - offset
+                            if type in ['data', '.data']:
+                                if not data['found']:
+                                    data['found'] = True
+                                    data['start'] = offset
+                                if type == 'data':
+                                    data['not_migrated'] = data['not_migrated'] + size
+                                elif type == '.data':
+                                    data['migrated'] = data['migrated'] + size
+                            elif type in ['rodata', '.rodata']:
+                                if not rodata['found']:
+                                    rodata['found'] = True
+                                    rodata['start'] = offset
+                                if type == 'rodata':
+                                    rodata['not_migrated'] = rodata['not_migrated'] + size
+                                elif type == '.rodata':
+                                    rodata['migrated'] = rodata['migrated'] + size
+                        data['size'] = data['migrated'] + data['not_migrated']
+                        data['end'] = data['start'] + data['size']
+                        writeDataBadges(segment['name'], 'data', data['migrated'], data['size'])
+                        rodata['size'] = rodata['migrated'] + rodata['not_migrated']
+                        rodata['end'] = rodata['start'] + rodata['size']
+                        writeDataBadges(segment['name'], 'rodata', rodata['migrated'], rodata['size'])
+
 def main(csv_name, version, overlay):
     with open(csv_name, mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
@@ -35,6 +91,7 @@ def main(csv_name, version, overlay):
         incomplete_func = 0
         if overlay == 'total':
             total_byte = sum(overlay_sizes.values())
+            computeDataOverlays()
         else:
             total_byte = overlay_sizes[overlay]
         incomplete_byte = 0
@@ -53,11 +110,10 @@ def main(csv_name, version, overlay):
             red = min(255, round(min(1, ((100 - percent) / 100) * 2) * 192))
             color = RGB_to_hex([red, green, 0])
             if overlay == 'total':
-                badge = anybadge.Badge("Donkey Kong 64 (US)", "%3.4f%%" % (percent), default_color=color)
+                badge = anybadge.Badge("Donkey Kong 64 (US)", str(total_func - incomplete_func) + "/" + str(total_func) + " functions - " + ("%3.4f%%" % (percent)), default_color=color)
             else:
-                badge = anybadge.Badge(overlay, "%3.4f%%" % (percent), default_color=color)
+                badge = anybadge.Badge(overlay, str(total_func - incomplete_func) + "/" + str(total_func) + " functions - " + ("%3.4f%%" % (percent)), default_color=color)
             badge.write_badge('progress/progress_' + overlay + '.svg',overwrite=True)
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Create progress csv based on map file',
