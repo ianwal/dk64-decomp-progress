1efa4ec9b6b8f50bb78227124ec2d598fdaf9eea
Donkey Kong <dk64.decomp@gmail.com>
2025-02-20 21:40:46 -0600

    52.3353%

--- src/global_asm/code_5E2F0.c
+++ src/global_asm/code_5E2F0.c
@@ -182,10 +182,10 @@ typedef struct {
     s16 unk30;
     s16 unk32;
     s16 unk34;
-    s8 unk36;
-    s8 unk37;
-    s8 unk38;
-    s8 unk39;
+    u8 unk36;
+    u8 unk37;
+    u8 unk38;
+    u8 unk39;
     s8 unk3A;
     s8 unk3B;
 } Struct807F7500;
@@ -232,13 +232,274 @@ void createLight(f32 arg0, f32 arg1, f32 arg2, f32 arg3, f32 arg4, f32 arg5, f32
     D_global_asm_807F7EE8 = 65.0f;
 }
 
-// Huge
+// Huge, close considering size
 #pragma GLOBAL_ASM("asm/nonmatchings/global_asm/code_5E2F0/func_global_asm_8065A884.s")
+
+typedef struct {
+    s32 unk0;
+    s32 unk4;
+    s32 unk8;
+    s32 unkC;
+    s32 unk10;
+    s32 unk14;
+    f32 unk18;
+    f32 unk1C;
+    f32 unk20;
+    s32 unk24;
+    s32 unk28;
+    s32 unk2C;
+    s16 unk30;
+    s16 unk32;
+} Struct8065C240;
+
+s32 func_global_asm_8065C240(Struct8065C240 *);
+extern s32 D_global_asm_80748304;
+extern s32 D_global_asm_80748308;
+
+/*
+void func_global_asm_8065A884(void) {
+    s32 i; // 154
+    s32 j;
+    s32 sp14C;
+    Actor *temp_s0;
+    f32 temp_f0_10;
+    f32 temp_f0_11;
+    f32 dz; // 13c
+    f32 dy; // 138
+    f32 dx; // 134
+    f32 temp_f20;
+    f32 temp_f18; // 12C
+    f32 temp_f16; // 128
+    f32 temp_f12; // 124
+    f32 temp_f22;
+    f32 temp_f26;
+    f32 temp_f28;
+    f32 temp_f2_5;
+    f32 temp_f30;
+    f32 var_f24_3; // 10C
+    s16 var_s3;
+    u8 spC8[0x40];
+    f32 spC4;
+    f32 spC0;
+    f32 var_f20_2;
+    f32 var_f22_3;
+    f32 var_f2;
+    f32 v;
+    f32 sq_temp;
+
+    sp14C = 0;
+    for (i = 0; i < D_global_asm_807FBB34; i++) {
+        spC8[i] = FALSE;
+    }
+    for (i = 0; (i < D_global_asm_80748300) && (cc_number_of_players < 3); i++) {
+        if ((func_global_asm_8065C240(&D_807F7500[i])) && (sp14C < 0xC)) {
+            sp14C++;
+            for (j = 0; j < D_global_asm_807FBB34; j++) {
+                temp_s0 = D_global_asm_807FB930[j].unk0;
+                if ((temp_s0->object_properties_bitfield & 0x2000)) continue;
+                switch (D_807F7500[i].unk39) {
+                    case 1:
+                        dx = D_807F7500[i].unk24 - D_807F7500[i].unk18;
+                        dy = D_807F7500[i].unk28 - D_807F7500[i].unk1C;
+                        dz = D_807F7500[i].unk2C - D_807F7500[i].unk20;
+                        sq_temp = sqrtf(SQ(dx) + SQ(dy) + SQ(dz));
+                        if (sq_temp == 0.0f) {
+                            sq_temp = 0.1f;
+                        }
+                        temp_f26 = dx / sq_temp;
+                        temp_f28 = dy / sq_temp;
+                        temp_f30 = dz / sq_temp;
+                        dx = temp_s0->x_position - D_807F7500[i].unk18;
+                        dy = temp_s0->y_position - D_807F7500[i].unk1C;
+                        dz = temp_s0->z_position - D_807F7500[i].unk20;
+                        sq_temp = sqrtf(SQ(dz) + (SQ(dx) + SQ(dy)));
+                        if (sq_temp == 0.0f) {
+                            sq_temp = 0.1f;
+                        }
+                        temp_f12 = (dx / sq_temp) * temp_f26;
+                        temp_f16 = (dy / sq_temp) * temp_f28;
+                        temp_f18 = (dz / sq_temp) * temp_f30;
+                        var_f22_3 = temp_f12 + temp_f16 + temp_f18;
+                        if (var_f22_3 < 0.0f) {
+                            var_f22_3 = 0.0f;
+                        }
+                        if (D_807F7500[i].unkC <= var_f22_3) {
+                            if (D_807F7500[i].unk4 <= var_f22_3) {
+                                var_f22_3 = 1.0f;
+                            } else {
+                                var_f22_3 = (var_f22_3 - D_807F7500[i].unkC) * D_807F7500[i].unk10;
+                            }
+                            addActorRecolor(temp_s0, 
+                                D_807F7500[i].unk18, 
+                                D_807F7500[i].unk1C, 
+                                D_807F7500[i].unk20, 
+                                0xFF, 
+                                D_807F7500[i].unk36 * var_f22_3, 
+                                D_807F7500[i].unk37 * var_f22_3, 
+                                D_807F7500[i].unk38 * var_f22_3, 0U);
+                            if ((temp_s0->object_properties_bitfield & 0x01000000) && (temp_s0->object_properties_bitfield & 4)) {
+                                sq_temp = sqrtf(
+                                    SQ(D_807F7500[i].unk18 - temp_s0->x_position) +
+                                    SQ(D_807F7500[i].unk20 - temp_s0->z_position) 
+                                    );
+                                var_f2 = sq_temp != 0.0f ? sq_temp : 0.001f;
+                                var_f24_3 = (D_807F7500[i].unk1C - temp_s0->y_position) / var_f2;
+                                if (!(var_f24_3 > 0.0f)) {
+                                    var_f24_3 = -var_f24_3;
+                                }
+                                if (var_f24_3 == 0.0f) {
+                                    var_f24_3 = 0.1f;
+                                }
+                                var_f24_3 = (temp_s0->unk15E * 0.4f) / var_f24_3;
+                                var_s3 = func_global_asm_80665DE0(
+                                    D_807F7500[i].unk18,
+                                    D_807F7500[i].unk20,
+                                    temp_s0->x_position,
+                                    temp_s0->z_position);
+                                if (temp_s0->animation_state != NULL) {
+                                    spC0 = temp_s0->animation_state->scale_x;
+                                    spC4 = temp_s0->animation_state->scale_z;
+                                } else {
+                                    spC0 = 0.15f;
+                                    spC4 = 0.15f;
+                                }
+                                v = temp_s0->shadow_opacity / 255.0;
+                                v = D_global_asm_80748304 * v;
+                                v *= var_f22_3;
+                                func_global_asm_8065D254(
+                                    temp_s0,
+                                    0x3C9,
+                                    0x40,
+                                    0x40,
+                                    spC0 * temp_s0->unk16E,
+                                    spC4 * temp_s0->unk16F,
+                                    1, 0x64,
+                                    v,
+                                    var_s3 + 0xC00, var_f24_3 * var_f22_3);
+                                spC8[j] = TRUE;
+                            }
+                        }
+                        break;
+                    case 0:
+                        var_f22_3 = sqrtf(
+                            SQ(D_807F7500[i].unk18 - temp_s0->x_position) + 
+                            SQ(D_807F7500[i].unk1C - temp_s0->y_position) +
+                            SQ(D_807F7500[i].unk20 - temp_s0->z_position) 
+                        );
+                        var_f22_3 -= temp_s0->unkCE;
+                        if (var_f22_3 < 0.0) {
+                            var_f22_3 = 0.1f;
+                        }
+                        temp_f20 = D_807F7500[i].unk14 * 1.3f;
+                        if ((temp_s0->object_properties_bitfield & 0x01000000) && (var_f22_3 < temp_f20) && (temp_s0->object_properties_bitfield & 4)) {
+                            sq_temp = sqrtf(
+                                SQ(D_807F7500[i].unk18 - temp_s0->x_position) +
+                                SQ(D_807F7500[i].unk20 - temp_s0->z_position));
+                            if (sq_temp == 0.0f) {
+                                sq_temp = 1.0f;
+                            }
+                            var_f24_3 = (D_807F7500[i].unk1C - temp_s0->y_position) / sq_temp;
+                            if (!(var_f24_3 > 0.0f)) {
+                                var_f24_3 = -var_f24_3;
+                            }
+                            if (var_f24_3 == 0.0f) {
+                                var_f24_3 = 0.1f;
+                            }
+                            var_f24_3 = (temp_s0->unk15E * 0.4f) / var_f24_3;
+                            var_s3 = func_global_asm_80665DE0(D_807F7500[i].unk18, D_807F7500[i].unk20, temp_s0->x_position, temp_s0->z_position);
+                            if (var_f24_3 > 10.0f) {
+                                var_f24_3 = 10.0f;
+                            }
+                            if (temp_s0->animation_state != NULL) {
+                                spC0 = temp_s0->animation_state->scale_x;
+                                spC4 = temp_s0->animation_state->scale_z;
+                            } else {
+                                spC0 = 0.15f;
+                                spC4 = 0.15f;
+                            }
+                            spC8[j] = TRUE;
+                        }
+                        temp_f0_10 = temp_f20 / 3.0f;
+                        if (var_f22_3 < temp_f0_10) {
+                            addActorRecolor(temp_s0,
+                                D_807F7500[i].unk18,
+                                D_807F7500[i].unk1C,
+                                D_807F7500[i].unk20, 
+                                0xFFU, 
+                                D_807F7500[i].unk36,
+                                D_807F7500[i].unk37, 
+                                D_807F7500[i].unk38, 0U);
+                            if ((temp_s0->object_properties_bitfield & 0x01000000) && (temp_s0->object_properties_bitfield & 4)) {
+                                v = temp_s0->shadow_opacity / 255.0;
+                                v = D_global_asm_80748304 * v;
+                                func_global_asm_8065D254(
+                                    temp_s0,
+                                    0x3C9,
+                                    0x40,
+                                    0x40,
+                                    spC0 * temp_s0->unk16E,
+                                    spC4 * temp_s0->unk16F,
+                                    1, 0x64,
+                                    v, var_s3 + 0xC00, var_f24_3);
+                            }
+                        } else {
+                            temp_f22 = var_f22_3 - temp_f0_10;
+                            temp_f2_5 = temp_f20 - temp_f0_10;
+                            if (temp_f22 < temp_f2_5) {
+                                temp_f0_11 = 1.0f - (temp_f22 / temp_f2_5);
+                                addActorRecolor(
+                                    temp_s0, 
+                                    D_807F7500[i].unk18, 
+                                    D_807F7500[i].unk1C, 
+                                    D_807F7500[i].unk20, 
+                                    0xFF, 
+                                    D_807F7500[i].unk36 * temp_f0_11, 
+                                    D_807F7500[i].unk37 * temp_f0_11, 
+                                    D_807F7500[i].unk38 * temp_f0_11, 0U);
+                                if ((temp_s0->object_properties_bitfield & 0x01000000) && (temp_s0->object_properties_bitfield & 4)) {
+                                    v = D_global_asm_80748304 * temp_f0_11 * (f32) (temp_s0->shadow_opacity / 255.0);
+                                    func_global_asm_8065D254(
+                                        temp_s0, 0x3C9, 0x40, 0x40, 
+                                        spC0 * temp_s0->unk16E, 
+                                        spC4 * temp_s0->unk16F, 
+                                        1, 0x64,
+                                        v, var_s3 + 0xC00,
+                                        var_f24_3 * temp_f0_11);
+                                }
+                            }
+                        }
+                        break;
+                }
+            }
+        }
+    }
+    for (i = 0; i < D_global_asm_807FBB34; i++) {
+        if (!spC8[i]) {
+            temp_s0 = D_global_asm_807FB930[i].unk0;
+            if ((temp_s0->object_properties_bitfield & 0x01000000) && (temp_s0->object_properties_bitfield & 4)) {
+                if (temp_s0->animation_state) {
+                    spC0 = temp_s0->animation_state->scale_x;
+                    spC4 = temp_s0->animation_state->scale_z;
+                } else {
+                    spC0 = 0.15f;
+                    spC4 = 0.15f;
+                }
+                v = D_global_asm_80748308 * (f32) (temp_s0->shadow_opacity / 255.0);
+                func_global_asm_8065D254(
+                    temp_s0, 0x3C9, 0x40, 0x40, 
+                    spC0 * temp_s0->unk16E, 
+                    spC4 * temp_s0->unk16F,
+                    1, 0x64,
+                    v, 0, 1.0f);
+            }
+        }
+    }
+}
+*/
+
 
 // Matrix stuff?
 #pragma GLOBAL_ASM("asm/nonmatchings/global_asm/code_5E2F0/func_global_asm_8065BAA0.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/global_asm/code_5E2F0/func_global_asm_8065BE74.s")
 
 typedef struct {
     f32 unk0;
@@ -265,46 +526,26 @@ typedef struct {
 extern Struct807F78C0 D_807F78C0[];
 extern Struct807F78C0 D_807F7BC8[];
 
-/*
-// TODO: Fiddly, decent progress made
 void func_global_asm_8065BE74(s16 arg0) {
     s32 i;
+    u8 temp2;
 
-    if (D_global_asm_807F7EC8 != 0xC) {
-        i = 0;
-        if (D_global_asm_807F7EC8 > 0) {
-            for (i = 0; i < D_global_asm_807F7EC8; i++) {
-                if (D_807F78C0[arg0].unk3F == D_807F7BC8[i].unk3F) {
-                    memcpy(&D_807F7BC8[D_global_asm_807F7EC8++], &D_807F78C0[arg0], sizeof(Struct807F78C0));
-                    break;
-                }
-            }
+    if (D_global_asm_807F7EC8 == 0xC) {
+        return;
+    }
+    temp2 = D_807F78C0[arg0].unk3F;
+    for (i = 0; i < D_global_asm_807F7EC8; i++) {
+        if (temp2 == D_807F7BC8[i].unk3F) {
+            return;
         }
     }
+    memcpy(&D_807F7BC8[D_global_asm_807F7EC8++], &D_807F78C0[arg0], sizeof(Struct807F78C0));
 }
-*/
 
 // Matrix stuff
 #pragma GLOBAL_ASM("asm/nonmatchings/global_asm/code_5E2F0/func_global_asm_8065BF18.s")
 
 extern u8 D_global_asm_80750AB4;
-
-typedef struct {
-    s32 unk0;
-    s32 unk4;
-    s32 unk8;
-    s32 unkC;
-    s32 unk10;
-    s32 unk14;
-    f32 unk18;
-    f32 unk1C;
-    f32 unk20;
-    s32 unk24;
-    s32 unk28;
-    s32 unk2C;
-    s16 unk30;
-    s16 unk32;
-} Struct8065C240;
 
 s32 func_global_asm_8065C240(Struct8065C240 *arg0) {
     f32 dz, dy, dx;
